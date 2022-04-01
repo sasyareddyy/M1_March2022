20fee4ae58ad5cdf3247d119060e14213c94cbc2
sasyareddyy <102736706+sasyareddyy@users.noreply.github.com>
2022-04-01 23:18:20 +0530

    Update reference.c

--- 2_implementation/src/reference.c
+++ 2_implementation/src/reference.c
@@ -1,1 +1,10 @@
-
+7 lines (7 sloc)  123 Bytes
+   
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+© 2022 GitHub, Inc.
