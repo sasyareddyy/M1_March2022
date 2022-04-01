ca163a779fc175ace5c355c8ebfc416ca3dcc4fe
sasyareddyy <102736706+sasyareddyy@users.noreply.github.com>
2022-04-01 23:21:47 +0530

    Update fun.h

--- 2_implementation/test/fun.h
+++ 2_implementation/test/fun.h
@@ -1,1 +1,15 @@
-
+13 lines (13 sloc)  168 Bytes
+   
+/**
+ * @file fun.h
+ * @brief 
+ * 
+ */
+int addition();
+int subtraction();
+int multiplication();
+int division();
+int modulus();
+int power();
+int factorial();
+int main();
