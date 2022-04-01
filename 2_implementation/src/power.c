6524bfff82f7680a54a407e9c24fc34547cbf393
sasyareddyy <102736706+sasyareddyy@users.noreply.github.com>
2022-04-01 23:16:57 +0530

    Update power.c

--- 2_implementation/src/power.c
+++ 2_implementation/src/power.c
@@ -1,1 +1,16 @@
+14 lines (11 sloc)  308 Bytes
+   
+#include<stdio.h>
+#include<math.h> // math support some mathemetics inbuilt function
+int power(int a, int b)
+{
+    int i=1;
+     int p; //declaring p 
+     while(i!=b){
+          p *=a; //calculating power (a,b) and storing in 'p'
+          return p; //returning power value to main funtion 
+  
 
+   
+     }
+}
